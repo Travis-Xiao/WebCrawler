b0fd1abaab215865fa0b0881034a9ce8384b2cd7
Travis-Xiao <Travis.Xiao@gmail.com>
2015-09-28 12:37:42 +0800

    a

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -19,7 +19,7 @@ namespace Crawler
 
             Policy p = new GooglePolicy();
             Crawler c = new Crawler(p);
-            c.PrepareQueries(keywords);
+            c.PrepareQueries(args);
             c.Start();
         }
     }
